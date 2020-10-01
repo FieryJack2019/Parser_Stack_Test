0447495e0231f520d65fb0c607be03b3173503e4
fiery_jack <adel.yakupov.80@gmail.com>
2020-10-01 21:37:52 +0300

    fix

---
+++ data_base.py
@@ -0,0 +1,92 @@
+import sqlite3
+from openpyxl import Workbook
+from random import randint
+
+def create_connect():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    return (conn, cursor)
+
+
+def insert_data(data):
+    con, cur = create_connect()
+    while True:
+        try:
+            tags = [i[0] for i in cur.execute("SELECT tag FROM data_parser WHERE 1").fetchall()]
+            break
+        except:
+            print("Error SQL: 'insert_data'")
+    if data[0] not in tags:
+        while True:
+            try:
+                cur.execute("INSERT INTO data_parser VALUES (?,?,?,?,?,?,?)", data)
+                break
+            except:
+                print("Error SQL: 'insert_data'")
+    con.commit()
+    con.close()
+
+def update_data(data):
+    con, cur = create_connect()
+    for key, value in data.items():
+        #while True:
+        #    try:
+                cur.execute(f"""UPDATE data_parser
+                            SET count_questions_tag = count_questions_tag + {value[0]} ,
+                                count_answer_tag = count_answer_tag + {value[1]} ,
+                                count_upvotes_tag = count_upvotes_tag + {value[2]} ,
+                                count_view_tag = count_view_tag + {value[3]} ,
+                                count_questions_owner_reputation_300 = count_questions_owner_reputation_300 + {value[4]}
+                            WHERE tag = '{key}'""")
+        #        break
+        #    except:
+        #        print("Error SQL: 'update_data'")
+    con.commit()
+    con.close()
+
+
+def new_data_base():
+    con, cur = create_connect()
+    cur.execute("""CREATE TABLE data_parser
+                  (tag text, 
+                   count_questions_tag integer, 
+                   count_answer_tag integer,
+                   count_upvotes_tag integer, 
+                   count_view_tag integer,
+                   count_questions_owner_reputation_300 integer,
+                   related_tags text)
+                   """)
+    con.commit()
+    con.close()
+
+
+def get_all_data():
+    con, cur = create_connect()
+    result = cur.execute("SELECT * FROM data_parser WHERE 1").fetchall()
+    wb = Workbook()
+    wb.create_sheet(title = 'Парсинг', index = 0)
+    sheet = wb['Парсинг']
+
+    sheet.append(['Тег', 'Кол-во вопросов', 'Кол-во ответов', 'Кол-во Upvotes', 'Кол-во views', 'Кол-во вопросов с owner rep > 300', 'Related tags'])
+
+    for item in result:
+        try:
+            sheet.append(item)
+        except:
+            pass
+
+    name_file = f'excel_main/stackoverflow_{randint(0, 99999999)}.xlsx'
+    wb.save(name_file)
+    return name_file
+
+
+
+def load_tags():
+    con, cur = create_connect()
+    result = cur.execute("SELECT tag, related_tags FROM data_parser WHERE 1").fetchall()
+    all_tag = [i[0] for i in result]
+    con.close()
+    return all_tag
+
+
+get_all_data()
---
+++ parserx_new.py
@@ -0,0 +1,130 @@
+from requests import get
+from time import sleep, time
+from json import loads
+import data_base
+
+
+def get_data(times):
+    tags = data_base.load_tags()
+
+    count_questions_tag = {}
+    count_questions_owner_reputation_300 = {}
+    count_answer_tag = {}
+    count_upvotes_tag = {}
+    count_view_tag = {}
+    related_tags = {}
+
+    for i in tags:
+        count_questions_tag[i] = 0
+        count_questions_owner_reputation_300[i] = 0
+        count_answer_tag[i] = 0
+        count_upvotes_tag[i] = 0
+        count_view_tag[i] = 0
+        related_tags[i] = ""
+    
+    page = 1
+    index = 1
+    api = "https://api.stackexchange.com/2.2/"
+    key = "21IxDPqUnDcVv5yn0xdZ)w(("
+
+    while True:
+        sleep(1)
+        try:
+            questions_data = loads(get(api + f"questions?page={page}&fromdate={times[0]}&todate={times[1]}&order=desc&sort=activity&site=stackoverflow&key={key}").text)
+            try:
+                print(questions_data["backoff"])
+            except:
+                pass       
+        except:
+            print("Error: get_question")
+            sleep(180)
+        for question in questions_data["items"]:
+            start_time = time()
+            tags_question = question["tags"]
+            for tag in tags_question:
+                count_questions_owner_reputation_300[tag] = 0
+                if tag not in tags:
+                    tags.append(tag)
+                try:
+                    count_questions_tag[tag] += 1
+                except KeyError:
+                    count_questions_tag[tag] = 1
+
+                try:
+                    count_answer_tag[tag] += question["answer_count"]
+                except KeyError:
+                    count_answer_tag[tag] = question["answer_count"]
+                        
+                try:
+                    count_upvotes_tag[tag] += question["score"]
+                except KeyError:
+                    count_upvotes_tag[tag] = question["score"]
+
+                try:
+                    count_view_tag[tag] += question["view_count"]
+                except KeyError:
+                    count_view_tag[tag] = question["view_count"]
+
+                try:
+                    if question["owner"]["reputation"] > 300:
+                        try:
+                            count_questions_owner_reputation_300[tag] += 1
+                        except KeyError:
+                            count_questions_owner_reputation_300[tag] = 1
+                except:
+                    pass
+                
+                if tag not in related_tags.keys():
+                    sleep(1)
+                    while True:
+                        try:
+                            tag_related = loads(get(api + f"tags/{tag}/related?site=stackoverflow&key={key}").text)
+                        except:
+                            print("Error: get_related_tag")
+                            sleep(180)
+                            continue
+                        try:
+                            tags_related = ", ".join([tag["name"] for tag in tag_related["items"][1:4]])
+                            related_tags[tag] = tags_related
+                        except:
+                            related_tags[tag] = ""
+                        break
+                    try:
+                        data_base.insert_data([tag, 
+                                               count_questions_tag[tag], 
+                                                count_answer_tag[tag], 
+                                                count_upvotes_tag[tag], 
+                                                count_view_tag[tag], 
+                                                count_questions_owner_reputation_300[tag], 
+                                                related_tags[tag]])
+                    except:
+                        data_base.insert_data([tag, 
+                                              count_questions_tag[tag], 
+                                              count_answer_tag[tag], 
+                                            count_upvotes_tag[tag], 
+                                            count_view_tag[tag], 
+                                            0, 
+                                            related_tags[tag]])
+            end_time = time()
+            print(f"Complete question [{index}], time: {end_time - start_time}")
+            index += 1
+
+        if questions_data["has_more"] == True:
+            page += 1
+        else:
+            break
+    sql_data_save([tags, count_questions_tag, 
+                        count_answer_tag, 
+                        count_upvotes_tag, 
+                        count_view_tag, 
+                        count_questions_owner_reputation_300])
+
+
+def sql_data_save(data):
+    data_special = {}
+    for tag in data[0]:
+        data_special[tag] = [item[tag] for item in data[1:]]
+    data_base.update_data(data_special)
+
+
+get_data([1598918400, 1599004800])
